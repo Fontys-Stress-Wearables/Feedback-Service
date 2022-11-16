32637b3884ae3ddedc9e9eed773499b20090de94
vcaf <victoria.fong.0408@gmail.com>
2022-11-16 18:45:18 +0100

    removed ?

--- src/Feedback-Service/Dtos/FeedbackDto.cs
+++ src/Feedback-Service/Dtos/FeedbackDto.cs
@@ -2,9 +2,9 @@ namespace Feedback_Service.Dtos;
 
 public record FeedbackDto(
     Guid Id,
-    int? PatientId,
-    Guid? AuthorId,
-    Guid? StressMeassurementId,
-    string Comment,
+    int PatientId,
+    Guid AuthorId,
+    Guid StressMeassurementId,
+    string? Comment,
     DateTimeOffset CreatedDate
 );
